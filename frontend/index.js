3f7700f06e27b939c8b1cebe7406edf7567eb8fc
swati-anchatageri <swatianchatageri129@gmail.com>
2023-10-21 01:29:03 +0530

    shayari generator

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,7 @@ let shayari=document.getElementById("shayari")
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
 console.log(value.value)
-fetch(`http://localhost:8080/shayari?keyword=${value.value}`)
+fetch(`https://shayari-backend-ganm.onrender.com/shayari?keyword=${value.value}`)
 .then((res)=>res.json())
 .then((data)=>{
     console.log(data)
